23ac32abffd5f921f34956c2321920ac15a65f6e
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-07-14 15:28:02 +0200

    correct dir

--- specifics/hannes/config.nix
+++ specifics/hannes/config.nix
@@ -9,7 +9,7 @@
   sops.secrets = {
     "gpg.key" = {
       format = "binary";
-      sopsFile = ./secrets/hannes/gpg.key;
+      sopsFile = ../../secrets/hannes/gpg.key;
       owner = "blingmember";
       mode = "0400";
       path = "/Users/blingmember/.gnupg/private-keys-v1.d/B36A4C516860D789671E2010E8A907F623EB6C32.key";
