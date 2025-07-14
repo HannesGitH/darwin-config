9ba177c64a048b7aa3d09452102a0670fba20642
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-07-14 15:23:02 +0200

    add my gpg key

--- specifics/hannes/config.nix
+++ specifics/hannes/config.nix
@@ -6,6 +6,16 @@
     prismlauncher
   ];
 
+  sops.secrets = {
+    "gpg.key" = {
+      format = "binary";
+      sopsFile = ./secrets/hannes/gpg.key;
+      owner = "blingmember";
+      mode = "0400";
+      path = "/Users/blingmember/.gnupg/private-keys-v1.d/B36A4C516860D789671E2010E8A907F623EB6C32.key";
+    };
+  };
+
   homebrew = {
     enable = true;
     casks = [
