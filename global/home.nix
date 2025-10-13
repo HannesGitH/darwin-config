27ae5b92095d80e881f56ee234ebae688294ccd9
Hannes <github@h-h.win>
2025-10-13 08:20:48 +0200

    add android home

--- global/home.nix
+++ global/home.nix
@@ -78,6 +78,9 @@
         . "$HOME/.cargo/env"
 
         export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+        export ANDROID_HOME="/Users/blingmember/Library/Android/sdk"
+        export PATH="$PATH:$ANDROID_HOME/tools"
+        export PATH="$PATH:$ANDROID_HOME/platform-tools"
       '';
     };
     programs.git = {
