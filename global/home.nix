ec3050e344fe64ecafff26417ebf846270959f66
Hannes <github@h-h.win>
2026-02-27 16:40:51 +0100

    git main branch name

--- global/home.nix
+++ global/home.nix
@@ -96,6 +96,7 @@
       enable = true;
       lfs.enable = true;
       extraConfig = {
+        init.defaultBranch = "main";
         push.autoSetupRemote = true;
         "mergetool \"vscode\"" = {
           cmd = "code --wait --merge $REMOTE $LOCAL $BASE $MERGED";
