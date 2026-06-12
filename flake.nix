8c11e82bcb78819e65ac7379c051085e19a4fb2d
Hannes <github@h-h.win>
2026-06-12 14:09:59 +0200

    use an improved zed

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       inputs.home-manager.follows = "home-manager";
     };
 
-    zed.url = "github:zed-industries/zed";
+    zed.url = "github:HannesGitH/zed";
 
     zsh-nix-shell = {
       url = "github:chisui/zsh-nix-shell/v0.7.0";
